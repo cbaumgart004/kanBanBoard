c5d30c48247d550b2b8055c708d52df5573a8094
Cbaumgart004 <baumgart.c@gmail.com>
2024-09-08 08:48:51 -0600

    Added a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,35 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("close")[0];
+const submitBtn = document.getElementById("submitBtn");
+btn.onclick = function() {
+    modal.style.display = "block";
+};
 
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+modal.style.display = "none";
+    }
+};
+
+    submitBtn.onclick = function() {
+    const input1Value = document.getElementById("input1").value;
+    const input2Value = document.getElementById("input2").value;
+    // Do something with the input values, e.g., send them to a server
+    console.log("Input 1:", input1Value);
+    console.log("Input 2:", input2Value);
+    modal.style.display = "none";
+};
+// TODO: create a function to generate a unique task id
+function generateTaskId() {
+    return nextId++;
 }
 
 // Todo: create a function to create a task card
@@ -19,7 +45,7 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    console.log('button clicked');
 }
 
 // Todo: create a function to handle deleting a task
@@ -36,3 +62,8 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+//Create a modal when the "Add Task" button is clicked
+    $('#addTaskButton').on('click', function() {
+        $('#addTaskModal').modal('show');
+    });

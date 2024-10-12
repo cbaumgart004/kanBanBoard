2127c7b7616248a7e81006dad9471edc96eb193c
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-11 19:13:50 -0600

    added readme, code cleanup--added comments and refactored to be  more DRY

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# kanBanBoard
-A Simple KanBan Board to keep tasks organized
+# Kan Ban Board
+
+## Description
+
+This is a simple KanBan board format that allows users, using local storage, to add, edit, and delete tasks. Users can also organize tasks by categories "To Do", "In Progress", and "Done". Note that all tasks that are overdue will turn red to alert the user that this task needs to be accomplished. All tasks due today are coded yellow. All tasks that are done turn green and get a little extra smiley face. Users can update the status of a task either by adjusting the dropdown menu OR dragging the task into the corresponding swim lane.
+?
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Kan Ban Board](#kan-ban-board)
+  - [Description](#description)
+  - [Table of Contents (Optional)](#table-of-contents-optional)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+## Installation
+
+No additional steps are required to run this application.
+
+## Usage
+
+Use the Add Task button to add a new Task. By Default, your new tasks will be assigned the status of "To Do". Add as many tasks as you like. You can modify the status of each task by either selecting the dropdown menu and reassigning, or by dragging the task itself into another column. Tasks that are due today will turn yellow, overdue tasks are red. All done tasks will turn green, as seen here.
+![here](assets/images/KanBan%20board%20usage.jpg)
+
+## Credits
+
+This project was built by [Chris Baumgart](https://github.com/cbaumgart004). It utilizes bootstrap for formatting purposes, and JQuery functionality.
+
+## License
+
+[MIT License](https://github.com/cbaumgart004/kanBanBoard/blob/main/LICENSE).
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,196 +1,196 @@
-// Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem('tasks')) || []
-let nextId = JSON.parse(localStorage.getItem('nextId')) || 0
+// Utility function to get and set localStorage
+function getLocalStorage(key, defaultValue) {
+  return JSON.parse(localStorage.getItem(key)) || defaultValue
+}
 
-const modal = document.getElementById('addTaskModal')
-const btn = document.getElementById('openModal')
-const span = document.getElementsByClassName('close')[0]
+function setLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+// Retrieve tasks and nextId from localStorage
+let taskList = getLocalStorage('tasks', [])
+let nextId = getLocalStorage('nextId', 0)
+
+// Common DOM element selectors
+const modal = $('#addTaskModal')
+const btn = $('#openModal')
+const span = $('.close')[0]
 let taskId = ''
 
-// TODO: create a function to generate a unique task id
+// Generate a unique task id
 function generateTaskId() {
   return nextId++
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// Render the task list and make cards draggable
 function renderTaskList() {
-  // Loop through the task list and create a task card for each task
   const $toDoList = $('#todo-cards')
   const $inProgressList = $('#in-progress-cards')
   const $doneList = $('#done-cards')
-  $toDoList.empty() // Clear all swim lanes
+  $toDoList.empty()
   $inProgressList.empty()
   $doneList.empty()
 
   taskList.forEach((task) => {
-    const taskElement = $(
-      `<div class="taskCard" id="task-${task.id}" draggable="true">
-                                <p>Id: ${task.id}</p>
-                                <h3>${task.name}</h3>
-                                <p>${task.description}</p>
-                                <select id="state${
-                                  task.id
-                                }" class="stateDropdown">
-                                    <option value="toDo" ${
-                                      task.state === 'toDo' ? 'selected' : ''
-                                    }>To Do</option>
-                                    <option value="inProgress" ${
-                                      task.state === 'inProgress'
-                                        ? 'selected'
-                                        : ''
-                                    }>In Progress</option>
-                                    <option value="done" ${
-                                      task.state === 'done' ? 'selected' : ''
-                                    }>Done</option>
-                                </select>
-                                <p>Due Date: ${dayjs(task.dueDate).format(
-                                  'MM/DD/YY'
-                                )}</p>
-                                <button class="btn editButton">Edit</button>
-                                <button class="btn deleteButton">Delete</button>
-                            </div>`
-    )
-    // Add color classes:
-    // Overdue tasks: red, Due Today: Yellow, Done Tasks: green
-    const taskDueDate = dayjs(task.dueDate)
-    const today = dayjs()
-    if (taskDueDate.isBefore(today, 'day')) {
-      taskElement.addClass('overdue')
-    } else if (taskDueDate.isSame(today, 'day')) {
-      taskElement.addClass('dueToday')
-    }
+    const taskElement = createTaskElement(task)
+    assignTaskColors(taskElement, task)
+
     if (task.state === 'toDo') {
       $toDoList.append(taskElement)
     } else if (task.state === 'inProgress') {
       $inProgressList.append(taskElement)
     } else if (task.state === 'done') {
       $doneList.append(taskElement)
-      taskElement.addClass('done')
-      // Remove any other classes
-      taskElement.removeClass('overdue')
-      taskElement.removeClass('dueToday')
-      // Add a smiley. Hooray!
-      taskElement.append('<span>&#128512;</span>')
+      finalizeDoneTask(taskElement)
     }
 
-    // Make the task card draggable
-    taskElement.draggable({
-      revert: 'invalid', // If not dropped in a droppable, revert to original position
-      start: function (event, ui) {
-        $(this).addClass('dragging')
-      },
-      stop: function (event, ui) {
-        $(this).removeClass('dragging')
-      },
-    })
+    makeTaskDraggable(taskElement)
   })
 }
 
-// Check if all fields are filled out before submitting the form
-function formValidation(event) {
-  if (!$('#input1').val()) {
-    $('#input1Error').text('Please fill out task title')
-  } else {
-    $('#input1Error').text('')
-  }
+// Create a task element
+function createTaskElement(task) {
+  return $(`
+      <div class="taskCard" id="task-${task.id}" draggable="true">
+        <h3>${task.name}</h3>
+        <p>${task.description}</p>
+        <select id="state${task.id}" class="stateDropdown">
+          <option value="toDo" ${
+            task.state === 'toDo' ? 'selected' : ''
+          }>To Do</option>
+          <option value="inProgress" ${
+            task.state === 'inProgress' ? 'selected' : ''
+          }>In Progress</option>
+          <option value="done" ${
+            task.state === 'done' ? 'selected' : ''
+          }>Done</option>
+        </select>
+        <p>Due Date: ${dayjs(task.dueDate).format('MM/DD/YY')}</p>
+        <button class="btn editButton">Edit</button>
+        <button class="btn deleteButton">Delete</button>
+      </div>
+    `)
+}
 
-  if (!$('#input2').val()) {
-    $('#input2Error').text('Please fill out task description')
-  } else {
-    $('#input2Error').text('')
-  }
+// Assign colors based on task due date
+function assignTaskColors(taskElement, task) {
+  const taskDueDate = dayjs(task.dueDate)
+  const today = dayjs()
 
-  if (!$('#datepicker').val()) {
-    $('#datepickerError').text('Please select a date')
-  } else {
-    $('#datepickerError').text('')
-  }
-
-  if ($('#input1').val() && $('#input2').val() && $('#datepicker').val()) {
-    console.log('Form submitted')
-    // If the form is complete, call the addTask function
-    $('#addTaskModal').hide()
-    handleAddTask(event)
+  if (taskDueDate.isBefore(today, 'day')) {
+    taskElement.addClass('overdue')
+  } else if (taskDueDate.isSame(today, 'day')) {
+    taskElement.addClass('dueToday')
   }
 }
 
-// Create a function to handle adding a new task
+// Finalize task appearance for "Done" tasks
+function finalizeDoneTask(taskElement) {
+  taskElement.addClass('done').removeClass('overdue dueToday')
+  taskElement.append('<span>&#128512;</span>')
+}
+
+// Make a task draggable
+function makeTaskDraggable(taskElement) {
+  taskElement.draggable({
+    revert: 'invalid',
+    start: function () {
+      $(this).addClass('dragging')
+    },
+    stop: function () {
+      $(this).removeClass('dragging')
+    },
+  })
+}
+
+// Validate form fields
+function formValidation(event) {
+  const fields = [
+    {
+      id: '#input1',
+      errorId: '#input1Error',
+      message: 'Please fill out task title',
+    },
+    {
+      id: '#input2',
+      errorId: '#input2Error',
+      message: 'Please fill out task description',
+    },
+    {
+      id: '#datepicker',
+      errorId: '#datepickerError',
+      message: 'Please select a date',
+    },
+  ]
+
+  let isValid = true
+  fields.forEach(({ id, errorId, message }) => {
+    if (!$(id).val()) {
+      $(errorId).text(message)
+      isValid = false
+    } else {
+      $(errorId).text('')
+    }
+  })
+
+  if (isValid) {
+    handleAddTask(event)
+    modal.hide()
+  }
+}
+
+// Handle adding a new task
 function handleAddTask(event) {
-  // Prevent form submission
   event.preventDefault()
-  // Get the input values
-  const input1Value = $('#input1').val()
-  const input2Value = $('#input2').val()
-  const datepickerValue = $('#datepicker').datepicker('getDate')
-  // Create a new task object
   const newTask = {
     id: generateTaskId(),
-    name: input1Value,
-    // By default, tasks are in the "toDo" status
+    name: $('#input1').val(),
     state: 'toDo',
-    description: input2Value,
-    dueDate: datepickerValue,
+    description: $('#input2').val(),
+    dueDate: $('#datepicker').datepicker('getDate'),
   }
-  // Add the task to the task list
-  taskList.push(newTask)
-  // Save the updated task list to localStorage
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-  localStorage.setItem('nextId', nextId)
-  // Clear the input fields
-  $('#input1').val('')
-  $('#input2').val('')
-  $('#datepicker').val('')
-  // Render the updated task list
-  renderTaskList()
-  // Hide the modals
-  console.log(newTask)
-}
-function handleEditTask(taskId) {
-  // Find the task in the task list using the taskId
-  const taskToEdit = taskList.find((task) => task.id === parseInt(taskId))
 
-  if (taskToEdit) {
-    // Populate the edit modal fields with the task details
-    $('#editInput1').val(taskToEdit.name) // Set task name
-    $('#editInput2').val(taskToEdit.description) // Set task description
-    $('#editDatepicker').datepicker('setDate', new Date(taskToEdit.dueDate)) // Set due date in datepicker
-  }
-}
-function handleDeleteTask(taskId) {
-  // Find the task in the task list using the taskId
-  const taskToDelete = taskList.find((task) => task.id === parseInt(taskId))
-  if (taskToDelete) {
-    // Remove the task from the task list
-    taskList = taskList.filter((task) => task.id !== parseInt(taskId))
-  }
-  // Save the updated task list to localStorage
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-  localStorage.setItem('nextId', nextId)
-  // Re-render the task list
+  taskList.push(newTask)
+  setLocalStorage('tasks', taskList)
+  setLocalStorage('nextId', nextId)
+
+  $('#input1, #input2, #datepicker').val('')
   renderTaskList()
 }
-// Function to handle dropping a task into a new status lane
+
+// Handle task editing
+function handleEditTask(taskId) {
+  const taskToEdit = taskList.find((task) => task.id === parseInt(taskId))
+  if (taskToEdit) {
+    $('#editInput1').val(taskToEdit.name)
+    $('#editInput2').val(taskToEdit.description)
+    $('#editDatepicker').datepicker('setDate', new Date(taskToEdit.dueDate))
+  }
+}
+
+// Handle task deletion
+function handleDeleteTask(taskId) {
+  taskList = taskList.filter((task) => task.id !== parseInt(taskId))
+  setLocalStorage('tasks', taskList)
+  renderTaskList()
+}
+
+// Handle task state change on drop
 function handleDrop(event, ui) {
   const cardId = $(ui.draggable).attr('id').split('-')[1]
   const newStatus = $(event.target).attr('id').split('-')[0]
-  console.log('Dropped card', cardId, 'into', newStatus)
-  // Update the task's state in the taskList array
   taskList.forEach((task) => {
     if (task.id === parseInt(cardId)) {
       task.state = newStatus
     }
   })
 
-  // Save the updated task list to localStorage
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-
-  // Re-render the task list
+  setLocalStorage('tasks', taskList)
   renderTaskList()
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// Event listener setup
 $(document).ready(function () {
-  // Call the functions to render the task list, add event listeners, make lanes droppable,
   renderTaskList()
 
   $('.lane').droppable({
@@ -198,131 +198,66 @@ $(document).ready(function () {
     drop: handleDrop,
     hoverClass: 'hovered',
   })
-})
 
-// Create a modal when the "Add Task" button is clicked
-// Event Listeners:
-// Add Task and Datepicker:
-$('#openModal').click(function () {
-  $('#addTaskModal').show()
-})
+  $('#openModal').click(() => modal.show())
+  span.onclick = () => modal.hide()
 
-span.onclick = function () {
-  $('#addTaskModal').hide()
-}
-
-// jQuery to hide the modal when clicking outside of it
-$(window).click(function (event) {
-  // Check if the clicked element is the modal
-  if ($(event.target).is('#editModal') || $(event.target).is('#addTaskModal')) {
-    // Hide the modal
-    $('#editModal').hide()
-    $('#addTaskModal').hide()
-  }
-})
-
-$(document).ready(function () {
-  $('#datepicker').click(function () {
-    $('#calendarModal').show()
+  $(window).click(function (event) {
+    if ($(event.target).is(modal)) {
+      modal.hide()
+    }
   })
-  $('#editDatepicker').click(function () {
-    $('#calendarModal').show()
+
+  $('#submitBtn').click(formValidation)
+  //update state dropdown
+  $(document).on('change', '.stateDropdown', function () {
+    const taskId = $(this).closest('.taskCard').attr('id').split('-')[1]
+    const selectedState = $(this).val()
+    taskList.forEach((task) => {
+      if (task.id === parseInt(taskId)) {
+        task.state = selectedState
+      }
+    })
+    setLocalStorage('tasks', taskList)
+    renderTaskList()
   })
-  $('.close').click(function () {
-    $('#calendarModal').hide()
+
+  $(document).on('click', '.editButton', function () {
+    taskId = $(this).closest('.taskCard').attr('id').split('-')[1]
+    $('#editDatepicker').datepicker()
+    $('#editModal').show()
+    handleEditTask(taskId)
+  })
+
+  $(document).on('click', '.deleteButton', function () {
+    taskId = $(this).closest('.taskCard').attr('id').split('-')[1]
+    $('#deleteModal').show()
+  })
+
+  $('#confirmDelete').click(function () {
+    $('#deleteModal').hide()
+    handleDeleteTask(taskId)
+  })
+
+  $('#cancelDelete').click(function () {
+    $('#deleteModal').hide()
+  })
+
+  $('#editSubmitBtn').click(function () {
     $('#editModal').hide()
-    console.log('Modal Closed')
+    const editTask = {
+      name: $('#editInput1').val(),
+      description: $('#editInput2').val(),
+      dueDate: $('#editDatepicker').datepicker('getDate'),
+    }
+    taskList.forEach((task) => {
+      if (task.id === parseInt(taskId)) {
+        Object.assign(task, editTask)
+      }
+    })
+    setLocalStorage('tasks', taskList)
+    renderTaskList()
   })
 
   $('#datepicker').datepicker()
-})
-
-// When the form is submitted, call the form validation function
-$('#submitBtn').click(formValidation)
-
-// Clear Storage when the "Clear Storage" button is clicked
-$('#clearLocalStorage').click(function () {
-  localStorage.clear()
-  nextId = 0
-  taskList = []
-  renderTaskList()
-})
-
-// Log all tasks when the "Log Tasks" button is clicked
-$('#logTasks').click(function () {
-  console.log('clicked Log Tasks')
-  console.log(taskList)
-})
-$('#recoverStates').click(function () {
-  console.log('clicked Recover States')
-  //set all states to 'toDo'
-  taskList.forEach((task) => {
-    task.state = 'toDo'
-  })
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-  renderTaskList()
-})
-// Event listener to change the task status when a dropdown is selected
-$(document).on('change', '.stateDropdown', function () {
-  console.log($(this).val())
-  // Find the task id associated with the dropdown and update the task status in the taskList array
-  taskId = $(this).closest('.taskCard').attr('id').split('-')[1]
-  const selectedState = $(this).val()
-  taskList.forEach((task) => {
-    if (task.id === parseInt(taskId)) {
-      task.state = selectedState
-    }
-  })
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-  renderTaskList()
-})
-
-// Event Listener to edit a task when an edit button is clicked
-$(document).on('click', '.editButton', function () {
-  taskId = $(this).closest('.taskCard').attr('id').split('-')[1]
-  console.log(taskId)
-  // Initialize the date picker with the current task due date
-  $('#editDatepicker').datepicker()
-  $('#editModal').show()
-  handleEditTask(taskId)
-})
-
-// Event listener to delete a task when a delete button is clicked
-$(document).on('click', '.deleteButton', function () {
-  taskId = $(this).closest('.taskCard').attr('id').split('-')[1]
-  $('#deleteModal').show()
-})
-
-$('#confirmDelete').click(function () {
-  $('#deleteModal').hide()
-  handleDeleteTask(taskId)
-})
-
-$('#cancelDelete').click(function () {
-  $('#deleteModal').hide()
-})
-
-// Submit the edited task when the "Save Changes" button is clicked
-$('#editSubmitBtn').click(function () {
-  $('#editModal').hide()
-
-  // Retrieve the updated input values
-  const editInput1Value = $('#editInput1').val()
-  const editInput2Value = $('#editInput2').val()
-  const editDatepickerValue = $('#editDatepicker').datepicker('getDate')
-
-  // Update the task details in the taskList array
-  taskList.forEach((task) => {
-    if (task.id === parseInt(taskId)) {
-      task.name = editInput1Value
-      task.description = editInput2Value
-      task.dueDate = editDatepickerValue
-    }
-  })
-
-  // Save the updated task list to localStorage
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-
-  // Re-render the task list
-  renderTaskList()
 })
